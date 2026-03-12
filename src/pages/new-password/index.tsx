187eb1adb7a9e39debd6bda2e09f8d1d8dfb5fe0
adil-ir <Dev.Adil@irsolutions.tech>
2026-03-12 10:18:08 +0500

    chat module updated

--- src/pages/new-password/index.tsx
+++ src/pages/new-password/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export const NewPasswordPage = () => {
   return (
-    <section className="w-full rounded-[26px] bg-white dark:bg-[#1D242A] p-7 xl:p-16">
+    <section className="w-full rounded-[26px] bg-white dark:bg-[#1D242A] p-6 lg:p-12 md:p-8 xl:p-16">
       <div className="xl:mb-22.75 lg:mb-14 md:mb-10 mb-6 flex items-center justify-between">
         <Link
           to="/forgot-password"
